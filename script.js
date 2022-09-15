9ac6da7c29cc2bd0b4bde0d19284f013e534c250
Lucas Martins <56271185+lucas-a-martins@users.noreply.github.com>
2022-09-15 17:43:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO!] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+             if (idade >= 0 && idade < 14) {
+                //criança
+                img.setAttribute('src', 'bebe_homem.jpg')
+             } else if (idade < 25) {
+                //jovem
+                img.setAttribute('src', 'jovem_homem.jpg')
+             } else if (idade < 60) {
+                //adulto
+                img.setAttribute('src', 'adulto_homem.jpg')
+             }else {
+                //idoso
+                img.setAttribute('src', 'idoso_homem.jpg')
+             }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 14) {
+                //criança
+                img.setAttribute('src', 'bebe_mulher.jpg')
+            } else if (idade < 25) {
+                //jovem
+                img.setAttribute('src', 'jovem_mulher.jpg')
+            } else if (idade < 60) {
+                //adulto
+                img.setAttribute('src', 'adulto_mulher.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso_mulher.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
